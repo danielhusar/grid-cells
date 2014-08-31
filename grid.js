48d142d12ba89fe023d2e7112019ba2f5961ad4a
Daniel Husar <dano.husar@gmail.com>
2014-08-31 17:30:53 +0100

    Update grid.js

--- grid.js
+++ grid.js
@@ -2,11 +2,15 @@
   'use strict';
 
   var APP = window.APP = function (config) {
+    //configuration
     this.canvas = document.querySelector('canvas');
     this.context = this.canvas.getContext('2d');
     this.config = config;
+    this.size = 30;
+    
     this.draw();
 
+    //next step click
     document.querySelector('button').addEventListener('click', function () {
       this.recalculate().draw();
     }.bind(this));
@@ -23,7 +27,6 @@
    */
   FN.draw = function () {
     var i, j;
-    var step = 30;
 
     this.context.clearRect(0, 0, 450, 450);
     this.context.fillStyle = '#00B2B3';
@@ -31,7 +34,7 @@
     for (i = 0; i < 15; i++) {
       for (j = 0; j < 15; j++) {
         var method = this.isAlive(j + 1, i + 1) ? 'drawFull' : 'drawEmpty';
-        this[method](i * step, j * step);
+        this[method](i * this.size, j * this.size);
       }
     }
 
@@ -48,10 +51,9 @@
   FN.recalculate = function () {
     var config = {};
     var i, j;
-    var step = 30;
 
-    for (i = 0; i < 15; i++) {
-      for (j = 0; j < 15; j++) {
+    for (i = 0; i < 15; i++;) {
+      for (j = 0; j < 15; j++;) {
         var x = i + 1;
         var y = j + 1;
         var current = this.isAlive(x, y);
@@ -75,16 +77,16 @@
    * @return int
    */
   FN.checkNeighbours = function (x, y) {
-    var config = {};
-    var i, j;
     var iMin = x - 1;
     var jMin = y - 1;
     var iTop = iMin + 3;
     var jTop = jMin + 3;
     var aliveCount = 0;
+    var config = {};
+    var i, j;
 
-    for (var i = iMin; i < iTop; i++) {
-      for (j = jMin; j < jTop; j++) {
+    for (var i = iMin; i < iTop; i++;) {
+      for (j = jMin; j < jTop; j++;) {
         if (this.isAlive(i, j)) {
           aliveCount = aliveCount + 1;
         }
@@ -101,11 +103,21 @@
    * @return {this}
    */
   FN.drawEmpty = function(x, y) {
-    this.context.rect(x, y, 30, 30);
+    this.context.rect(x, y, this.size, this.size);
     return this;
   };
 
-
+  /**
+   * Draw filled rectangle
+   * @param  {int} x coord
+   * @param  {int} y coord
+   * @return {this}
+   */
+  FN.drawFull = function(x, y) {
+    this.context.fillRect(x, y, this.size, this.size);
+    return this;
+  };
+  
   /**
    * Check if the cell is alive or dead
    * @param {int} x coord
@@ -116,19 +128,9 @@
     return !! (this.config[x] && this.config[x].indexOf(y) !== -1);
   };
 
-  /**
-   * Draw filled rectangle
-   * @param  {int} x coord
-   * @param  {int} y coord
-   * @return {this}
-   */
-  FN.drawFull = function(x, y) {
-    this.context.fillRect(x, y, 30,30);
-    return this;
-  };
-
 
   var config = {
+    1 : [],
     2 : [2, 6, 8, 12, 13, 14],
     3 : [3, 4, 8, 9, 11],
     4 : [5, 7, 10, 12],
